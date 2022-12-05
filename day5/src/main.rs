532bb8d2f45b4a056cdeddb4eabbf3827154bc4e
Lily <ratshate22@gmail.com>
2022-12-05 07:53:44 +0000

    Day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -8,11 +8,19 @@ fn main() {
 fn part1(input: &str) -> String {
     let mut crane: HashMap<usize, Vec<char>> = HashMap::new();
     let mut input = input.split("\n\n");
-    let _ = input.next().unwrap().lines().for_each(|line|{
-        for symbol in line.chars().enumerate().filter(|char| char.1.is_alphabetic()).collect::<Vec<(usize, char)>>() {
-            let mut input = crane.get(&((symbol.0 - 1)/ 4)).unwrap_or(&Vec::new()).clone();
+    let _ = input.next().unwrap().lines().for_each(|line| {
+        for symbol in line
+            .chars()
+            .enumerate()
+            .filter(|char| char.1.is_alphabetic())
+            .collect::<Vec<(usize, char)>>()
+        {
+            let mut input = crane
+                .get(&((symbol.0 - 1) / 4))
+                .unwrap_or(&Vec::new())
+                .clone();
             input.push(symbol.1);
-            crane.insert((symbol.0-1)/4, input);
+            crane.insert((symbol.0 - 1) / 4, input);
         }
     });
     crane.clone().iter().for_each(|item| {
@@ -24,20 +32,21 @@ fn part1(input: &str) -> String {
         .next()
         .unwrap()
         .lines()
-        .map(|line| line
-             .split(' ')
-             .enumerate()
-             .filter(|symbol| symbol.0 % 2 == 1)
-             .map(|symbol| symbol.1.parse::<usize>().unwrap())
-             .collect::<Vec<usize>>()
-        ).for_each(|item| {
+        .map(|line| {
+            line.split(' ')
+                .enumerate()
+                .filter(|symbol| symbol.0 % 2 == 1)
+                .map(|symbol| symbol.1.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .for_each(|item| {
             let mut input = crane[&(item[1] - 1)].clone();
-            let mut output = crane[&(item[2]- 1)].clone();
+            let mut output = crane[&(item[2] - 1)].clone();
             for _ in 0..item[0] {
                 output.push(input.pop().unwrap())
             }
-            crane.insert(item[1]-1, input);
-            crane.insert(item[2]-1, output);
+            crane.insert(item[1] - 1, input);
+            crane.insert(item[2] - 1, output);
         });
     let mut output = String::new();
     for i in 0..crane.len() {
@@ -49,11 +58,19 @@ fn part1(input: &str) -> String {
 fn part2(input: &str) -> String {
     let mut crane: HashMap<usize, Vec<char>> = HashMap::new();
     let mut input = input.split("\n\n");
-    let _ = input.next().unwrap().lines().for_each(|line|{
-        for symbol in line.chars().enumerate().filter(|char| char.1.is_alphabetic()).collect::<Vec<(usize, char)>>() {
-            let mut input = crane.get(&((symbol.0 - 1)/ 4)).unwrap_or(&Vec::new()).clone();
+    let _ = input.next().unwrap().lines().for_each(|line| {
+        for symbol in line
+            .chars()
+            .enumerate()
+            .filter(|char| char.1.is_alphabetic())
+            .collect::<Vec<(usize, char)>>()
+        {
+            let mut input = crane
+                .get(&((symbol.0 - 1) / 4))
+                .unwrap_or(&Vec::new())
+                .clone();
             input.push(symbol.1);
-            crane.insert((symbol.0-1)/4, input);
+            crane.insert((symbol.0 - 1) / 4, input);
         }
     });
     crane.clone().iter().for_each(|item| {
@@ -65,19 +82,20 @@ fn part2(input: &str) -> String {
         .next()
         .unwrap()
         .lines()
-        .map(|line| line
-             .split(' ')
-             .enumerate()
-             .filter(|symbol| symbol.0 % 2 == 1)
-             .map(|symbol| symbol.1.parse::<usize>().unwrap())
-             .collect::<Vec<usize>>()
-        ).for_each(|item| {
+        .map(|line| {
+            line.split(' ')
+                .enumerate()
+                .filter(|symbol| symbol.0 % 2 == 1)
+                .map(|symbol| symbol.1.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .for_each(|item| {
             let input = crane[&(item[1] - 1)].clone();
             let input = input.split_at(crane[&(item[1] - 1)].len() - item[0]);
-            let mut output = crane[&(item[2]- 1)].clone();
-            crane.insert(item[1]-1, input.0.to_vec());
+            let mut output = crane[&(item[2] - 1)].clone();
+            crane.insert(item[1] - 1, input.0.to_vec());
             output.append(&mut input.1.to_vec());
-            crane.insert(item[2]-1, output);
+            crane.insert(item[2] - 1, output);
         });
     let mut output = String::new();
     for i in 0..crane.len() {
