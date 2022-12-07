a9454bf0cc19b49de36f95e66e3b8256aa684f4b
Lily <ratshate22@gmail.com>
2022-12-07 20:20:31 +1000

    Day7 Part2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -32,4 +32,29 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
+    let mut path = vec![""];
+    let mut directories: HashMap<String, usize> = HashMap::new();
+    let mut root_val = 0;
+    input.lines().for_each(|line|{
+        if line.starts_with("$ cd ..") {
+            let path_val = *directories.get(&path.join("/")).unwrap();
+            path.pop();
+            *directories.get_mut(&path.join("/")).unwrap() += path_val;
+        } else if line.starts_with("$ cd") {
+            let new_path = line.split(" ").skip(2).next().unwrap();
+            let item = *directories.get(&format!("{}/{}", path.join("/"), new_path).to_string()).unwrap_or(&0);
+            *directories.get_mut(&path.join("/")).unwrap_or(&mut 0) -= item;
+            path.push(new_path);
+            directories.insert(path.join("/"), item);
+        } else if !line.starts_with("$") && !line.starts_with("dir") {
+            *directories.get_mut(&path.join("/")).unwrap() += line.split(" ").next().unwrap().parse::<usize>().unwrap();
+            root_val += line.split(" ").next().unwrap().parse::<usize>().unwrap();
+        }
+    });
+    directories.remove(&"//".to_string());
+    directories.insert("//".to_string(), root_val);
+    let val = directories.values().max().unwrap();
+    let mut directories = directories.values().map(|value| *value).collect::<Vec<usize>>();
+    directories.sort();
+    *directories.iter().filter(|value| **value >= val - 40000000).next().unwrap()
 }
